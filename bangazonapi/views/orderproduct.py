5a303f0c6157543c59585e345cc9fb5d128eebd6
Daniel Krusch <daniel.krusch@gmail.com>
2019-10-04 14:30:37 -0500

    Can now post to order products

--- bangazonapi/views/orderproduct.py
+++ bangazonapi/views/orderproduct.py
@@ -10,7 +10,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from bangazonapi.models import OrderProduct
+from bangazonapi.models import OrderProduct, Order, Product
 
 
 class OrderProductSerializer(serializers.HyperlinkedModelSerializer):
