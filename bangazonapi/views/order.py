48573b3d014d76cefdd2fbef42303bdbaa7e0944
Tyler Carpenter <tcarp55@gmail.com>
2019-10-04 10:07:15 -0500

    starting to work

--- bangazonapi/views/order.py
+++ bangazonapi/views/order.py
@@ -4,7 +4,6 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from bangazonapi.models import ParkArea, Attraction
 
 
 class ParkAreaSerializer(serializers.HyperlinkedModelSerializer):
