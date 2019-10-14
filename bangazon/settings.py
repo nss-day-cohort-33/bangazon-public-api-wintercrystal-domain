a82a86f49efce8956455d1395fcb35bce3242b5b
Mary West <westmarye1219@gmail.com>
2019-10-14 13:22:32 -0500

    wip

--- bangazon/settings.py
+++ bangazon/settings.py
@@ -68,7 +68,9 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'http://localhost:3002',
+    'http://127.0.0.1:3002'
 )
 
 ROOT_URLCONF = 'bangazon.urls'
