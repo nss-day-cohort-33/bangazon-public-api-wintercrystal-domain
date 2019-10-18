1526908222ca43ad6ee8b693987565e8b3864e94
Dustin Hobson <dhobson21@gmail.com>
2019-10-18 13:35:19 -0500

    added avg_rating method to customer class and view

--- bangazonapi/models/customer.py
+++ bangazonapi/models/customer.py
@@ -8,6 +8,22 @@ class Customer(models.Model):
     phone_number = models.CharField(max_length=15)
     address = models.CharField(max_length=55)
 
+
+    @property
+    def avg_rating(self):
+        ratings =self.customer_rating.all()
+        total_score = 0
+        if len(ratings) > 0:
+            for score in ratings:
+                total_score += score.score
+            total_score = +total_score/len(ratings)
+        else:
+            total_score = 0
+
+
+        return total_score
+
+
     class Meta:
         verbose_name = ("customer")
         verbose_name_plural = ("customers")
